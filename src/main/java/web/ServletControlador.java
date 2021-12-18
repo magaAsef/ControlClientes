0028df63f616d55f47608b35fd730f05871f3c78
magaAsef <magali.asef@hotmail.com>
2021-12-18 16:49:11 -0300

    Se agrega el servlet controlador y la parte de interfaz de usuario creada con bootstrap y font awesome

---
+++ src/main/java/web/ServletControlador.java
@@ -0,0 +1,36 @@
+package web;
+
+import datos.ClienteDaoJDBC;
+import dominio.Cliente;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/ServletControlador")
+public class ServletControlador extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        List<Cliente> clientes = new ClienteDaoJDBC().listar();
+        System.out.println("Clientes: " + clientes);
+        request.setAttribute("clientes", clientes);
+        request.setAttribute("totalClientes", clientes.size());
+        request.setAttribute("saldoTotal", this.calcularSaldoTotal(clientes));
+        
+        request.getRequestDispatcher("clientes.jsp").forward(request, response);
+    }
+    
+    private double calcularSaldoTotal(List<Cliente> clientes){
+        double total = 0;
+        for(Cliente cliente: clientes){
+            total = total + cliente.getSaldo();
+        }
+        return total;
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+        
+    }
+}
